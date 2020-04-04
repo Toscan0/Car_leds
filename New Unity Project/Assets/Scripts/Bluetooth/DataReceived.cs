f0fd9e29b348f2dd5dbd30aa2282868d48f4d85c
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-04 20:56:34 +0100

    msg received from arduino done

---
+++ New Unity Project/Assets/Scripts/Bluetooth/DataReceived.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArduinoBluetoothAPI;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Text))]
+public class DataReceived : MonoBehaviour
+{
+    public BluetoothManager btManager;
+    
+    private BluetoothHelper helper;
+    private string outMsg = "";
+    private Text text;
+
+    void Start()
+    {
+        helper = btManager.GetBluetoothHelper();
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (btManager.connectBT)
+        {
+            outMsg = helper.Read();
+            UpdateText();
+        }
+    }
+
+    private void UpdateText()
+    {
+        text.text += outMsg + " ";
+    }
+}
