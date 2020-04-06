2e369d3e231b0d3d31ce10332c2d527e10369b60
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-06 18:48:52 +0100

    ble is working

--- car_leds/Assets/Scripts/BLE/BLEManager.cs
+++ car_leds/Assets/Scripts/BLE/BLEManager.cs
@@ -15,51 +15,7 @@ public class BLEManager : MonoBehaviour
 
     void Start()
     {
-        timer = 0;
-        try{
-            Debug.Log("HI");
-            BluetoothHelper.BLE = true;  //use Bluetooth Low Energy Technology
-            bluetoothHelper = BluetoothHelper.GetInstance("TEST");
-            Debug.Log(bluetoothHelper.getDeviceName());
-            bluetoothHelper.OnConnected += () => {
-                Debug.Log("Connected");
-                sendData();
-            };
-            bluetoothHelper.OnConnectionFailed += ()=>{
-                Debug.Log("Connection failed");
-            };
-            bluetoothHelper.OnScanEnded += OnScanEnded;
-            bluetoothHelper.OnServiceNotFound += (serviceName) =>
-            {
-                Debug.Log(serviceName);
-            };
-            bluetoothHelper.OnCharacteristicNotFound += (serviceName, characteristicName) =>
-            {
-                Debug.Log(characteristicName);
-            };
-            bluetoothHelper.OnCharacteristicChanged += (value, characteristic) =>
-            {
-                Debug.Log(characteristic.getName());
-                Debug.Log(System.Text.Encoding.ASCII.GetString(value));
-            };
-
-            // BluetoothHelperService service = new BluetoothHelperService("FFE0");
-            // service.addCharacteristic(new BluetoothHelperCharacteristic("FFE1"));
-            // BluetoothHelperService service2 = new BluetoothHelperService("180A");
-            // service.addCharacteristic(new BluetoothHelperCharacteristic("2A24"));
-            // bluetoothHelper.Subscribe(service);
-            // bluetoothHelper.Subscribe(service2);
-            // bluetoothHelper.ScanNearbyDevices();
-
-            BluetoothHelperService service = new BluetoothHelperService("19B10000-E8F2-537E-4F6C-D104768A1214");
-            service.addCharacteristic(new BluetoothHelperCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214"));
-            bluetoothHelper.Subscribe(service);
-            //bluetoothHelper.Subscribe(service2);
-            bluetoothHelper.ScanNearbyDevices();
-
-        }catch(Exception ex){
-            Debug.LogError(ex.Message);
-        }
+        TryToConnect();
     }
 
     private void OnScanEnded(LinkedList<BluetoothDevice> devices){
@@ -71,13 +27,14 @@ public class BLEManager : MonoBehaviour
             
         try
         {
-            bluetoothHelper.setDeviceName("KURV_Master");
+            bluetoothHelper.setDeviceName("ASTRA_K_LED_BLE");
             // bluetoothHelper.setDeviceName("HC-08");
             bluetoothHelper.Connect();
             Debug.Log("Connecting");
         }catch(Exception ex)
         {
             Debug.Log(ex.Message);
+            exception.text += ex + " ";
         }
 
     }
@@ -98,22 +55,25 @@ public class BLEManager : MonoBehaviour
         if(timer < 5)
             return;
         timer = 0;
-        sendData();
+        SendData();
     }
 
-    void sendData(){
+    void SendData(){
         // Debug.Log("Sending");
         // BluetoothHelperCharacteristic ch = new BluetoothHelperCharacteristic("FFE1");
         // ch.setService("FFE0"); //this line is mandatory!!!
         // bluetoothHelper.WriteCharacteristic(ch, new byte[]{0x44, 0x55, 0xff});
 
         Debug.Log("Sending");
-        BluetoothHelperCharacteristic ch = new BluetoothHelperCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214");
-        ch.setService("19B10000-E8F2-537E-4F6C-D104768A1214"); //this line is mandatory!!!
-        bluetoothHelper.WriteCharacteristic(ch, "10001000"); //string: 10001000 is this binary? no, as string.
+        msg.text += " --- ";
+        BluetoothHelperCharacteristic ch = new BluetoothHelperCharacteristic("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+        ch.setService("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"); //this line is mandatory!!!
+        bluetoothHelper.WriteCharacteristic(ch, "128"); //string: 10001000 is this binary? no, as string.
+
+        msg.text += " SENDIG ";
     }
 
-    void read(){
+    void Read(){
         BluetoothHelperCharacteristic ch = new BluetoothHelperCharacteristic("2A24");
         ch.setService("180A");//this line is mandatory!!!
 
@@ -121,4 +81,63 @@ public class BLEManager : MonoBehaviour
         //Debug.Log(System.Text.Encoding.ASCII.GetString(x));
     }
 
+    private void TryToConnect()
+    {
+        timer = 0;
+        try
+        {
+            Debug.Log("HI");
+            msg.text = "HI ";
+            BluetoothHelper.BLE = true;  //use Bluetooth Low Energy Technology
+            bluetoothHelper = BluetoothHelper.GetInstance("TEST");
+            Debug.Log(bluetoothHelper.getDeviceName());
+            msg.text += bluetoothHelper.getDeviceName() + " ";
+            bluetoothHelper.OnConnected += () => {
+                Debug.Log("Connected");
+                msg.text += "Connected ";
+                SendData();
+            };
+            bluetoothHelper.OnConnectionFailed += () => {
+                Debug.Log("Connection failed");
+                msg.text += "Connection failed ";
+            };
+            bluetoothHelper.OnScanEnded += OnScanEnded;
+            bluetoothHelper.OnServiceNotFound += (serviceName) =>
+            {
+                Debug.Log(serviceName);
+                msg.text += serviceName + " ";
+            };
+            bluetoothHelper.OnCharacteristicNotFound += (serviceName, characteristicName) =>
+            {
+                Debug.Log(characteristicName);
+                msg.text += characteristicName + " ";
+            };
+            bluetoothHelper.OnCharacteristicChanged += (value, characteristic) =>
+            {
+                Debug.Log(characteristic.getName());
+                Debug.Log(System.Text.Encoding.ASCII.GetString(value));
+                msg.text += characteristic.getName() + " " + System.Text.Encoding.ASCII.GetString(value) + " ";
+            };
+
+            // BluetoothHelperService service = new BluetoothHelperService("FFE0");
+            // service.addCharacteristic(new BluetoothHelperCharacteristic("FFE1"));
+            // BluetoothHelperService service2 = new BluetoothHelperService("180A");
+            // service.addCharacteristic(new BluetoothHelperCharacteristic("2A24"));
+            // bluetoothHelper.Subscribe(service);
+            // bluetoothHelper.Subscribe(service2);
+            // bluetoothHelper.ScanNearbyDevices();
+
+            BluetoothHelperService service = new BluetoothHelperService("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+            service.addCharacteristic(new BluetoothHelperCharacteristic("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"));
+            bluetoothHelper.Subscribe(service);
+            //bluetoothHelper.Subscribe(service2);
+            bluetoothHelper.ScanNearbyDevices();
+
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+            exception.text += ex + " ";
+        }
+    }
 }
