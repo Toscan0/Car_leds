f7494898272272c116f1a7c740f5b1be7d789a36
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-10 16:38:51 +0100

    BlE arduino code changed

--- arduino/car_leds/ESP32_BLE_UART_Demo/ESP32_BLE_UART_Demo.ino
+++ arduino/car_leds/ESP32_BLE_UART_Demo/ESP32_BLE_UART_Demo.ino
@@ -5,7 +5,7 @@
 
    Create a BLE server that, once we receive a connection, will send periodic notifications.
    The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
    Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
 
    The design of creating the BLE server is:
@@ -17,102 +17,103 @@
    6. Start advertising.
 
    In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second.
+   And txValue is the data to be sent, in this example just a byte incremented every second. 
 */
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLECharacteristic *pCharacteristic;
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
 bool deviceConnected = false;
-float txValue = 0;
-const int readPin = 32; // Use GPIO number. See ESP32 board pinouts
-const int LED = 22; // Could be different depending on the dev board. I used the DOIT ESP32 dev board.
+bool oldDeviceConnected = false;
+uint8_t txValue[50] = {0};
 String inString = "";
+const int LED = 22; // Could be different depending on the dev board. I used the DOIT ESP32 dev board.
 
-//std::string rxValue; // Could also make this a global var to access it in loop()
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // receive characteristic from unity  arduino -> unity
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" //write characteristic from unity unity -> arduino
+
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
+      Serial.println("CLIENT CONECTED");
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
+      Serial.println("CLIENT DISCONECTED");
     }
 };
 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
-
+     std::string rxValue = pCharacteristic->getValue();
+      
       if (rxValue.length() > 0) {
         Serial.println("*********");
         Serial.print("Received Value: ");
-				
-				inString="";
-        for (int i = 0; i < rxValue.length(); i++) {
+
+        inString="";
+        for (int i = 0; i < rxValue.length(); i++){
           Serial.print(rxValue[i]);
-					 inString += (char)rxValue[i];
+          txValue[i]=rxValue[i];
+          inString += (char)rxValue[i];
         }
-
         Serial.println();
-				
-				//if(inString.toInt()){
-					int pwm_led=inString.toInt();
-					Serial.print("pwm");
-					Serial.println(pwm_led);
-					ledcWrite(1, pwm_led);
-				//}
-				inString="";
 
-        Serial.println();
-        Serial.println("*********");
+        int pwm_led=inString.toInt();
+        Serial.print("pwm");
+        Serial.println(pwm_led);
+        ledcWrite(1, pwm_led);
+        Serial.println("***\n\n");
+
+        pTxCharacteristic->setValue(txValue,rxValue.length());
+        pTxCharacteristic->notify();
+//        txValue++;
       }
     }
 };
 
+
 void setup() {
   Serial.begin(115200);
-
-  //pinMode(LED, OUTPUT);
+  
   ledcAttachPin(LED, 1);
   ledcSetup(1, 300, 8); // 12 kHz PWM, 8-bit resolution
-	ledcWrite(1, 255);//start at full bright
-
+  ledcWrite(1, 255);//start at full bright
 
   // Create the BLE Device
-  BLEDevice::init("ASTRA_K_LED"); // Give it a name
+  BLEDevice::init("ASTRA_K_LED_BLE");
 
   // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
+  pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID_TX,
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );
+  pTxCharacteristic = pService->createCharacteristic(
+										CHARACTERISTIC_UUID_RX,
+										BLECharacteristic::PROPERTY_NOTIFY
+									);
+                      
+  pTxCharacteristic->addDescriptor(new BLE2902());
 
-  pCharacteristic->addDescriptor(new BLE2902());
+  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+											 CHARACTERISTIC_UUID_TX,
+											BLECharacteristic::PROPERTY_WRITE
+										);
 
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID_RX,
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-
-  pCharacteristic->setCallbacks(new MyCallbacks());
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
 
   // Start the service
   pService->start();
@@ -123,9 +124,22 @@ void setup() {
 }
 
 void loop() {
-  if (deviceConnected) {
 
-  }
-  delay(1000);
-	  //Serial.println("Waiting a client connection to notify...");
+    if (deviceConnected) {
+
+		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+	}
+
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+		// do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
