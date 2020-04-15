a8ea7310938df58bdef10c86d005e0f9d1277279
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-15 19:29:51 +0100

    Bluettoth related files added

---
+++ car_leds/Assets/Scripts/Bluetooth/DataReceived.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArduinoBluetoothAPI;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Text))]
+public class DataReceived : MonoBehaviour
+{
+    public BluetoothManager btManager;
+    
+    private BluetoothHelper helper;
+    private string outMsg = "";
+    private Text text;
+
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (btManager.connectBT == true)
+        {
+            //Read from bluetooth
+            helper = btManager.GetBluetoothHelper();
+            outMsg = helper.Read();
+
+            ParseMsg();
+        }
+    }
+
+    private void ParseMsg()
+    {
+        int indexWhiteSpace;
+
+        outMsg = outMsg.Trim();
+
+        if (outMsg != "")
+        {
+            //first white space fouded, that will be betwen two words, thx to the Trim function
+            indexWhiteSpace = outMsg.IndexOf(" "); 
+
+            outMsg = outMsg.Remove(0, indexWhiteSpace);
+
+            UpdateText();
+        }
+    }
+
+    private void UpdateText()
+    {
+        text.text += outMsg + " ";
+    }
+}
---
+++ car_leds/Assets/Scripts/Bluetooth/UI/UISlider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArduinoBluetoothAPI;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UISlider : MonoBehaviour
+{
+    public BluetoothManager btManager;
+    public Text text;
+
+    private BluetoothHelper helper;
+
+    private void Start()
+    {
+        text.text = this.gameObject.GetComponent<Slider>().value.ToString();
+    }
+
+    public void SliderChanged(float n)
+    {
+        string value = n.ToString();
+
+        if (btManager.connectBT == true)
+        {
+            helper = btManager.GetBluetoothHelper(); //refact this to start
+            helper.SendData(value);
+        }
+        Debug.Log(n);
+        text.text = value;
+    }
+}
